e44e660bc0d5e50a8e6e5103cd688f9d2176687e
Riki Ham <rikiham2@gmail.com>
2020-11-01 22:03:50 +0100

    fixed UserService

--- src/main/java/com/example/pokeApi/services/UserService.java
+++ src/main/java/com/example/pokeApi/services/UserService.java
@@ -8,6 +8,7 @@ import com.example.pokeApi.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,10 +28,10 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
     @GetMapping
-    public List<User> findAll(String user){
+    public List<User> findAll(String user) {
         var users = userRepository.findAll();
 
-        if(user!=null){
+        if (user != null) {
             users = users.stream()
                     .filter(b -> b.getUsername()
                             .toLowerCase()
@@ -47,12 +48,38 @@ public class UserService {
     }
 
     @PutMapping
-    public User save(User user){
-        if(user == null){
+    public User save(User user) {
+        if (user == null) {
             throw new ResponseStatusException(HttpStatus.NO_CONTENT);
 
         }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
+
+
+
+
+
+
+    public void update(String id, User user){
+        var isAdmin = SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().toUpperCase().equals("ROLE_ADMIN"));
+        var isCurrentUser = SecurityContextHolder.getContext().getAuthentication()
+                .getName().toLowerCase().equals(user.getUsername().toLowerCase());
+        if (!isAdmin && !isCurrentUser){
+            log.warn("Attempt to update another user made.");
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "You can only update yourself");
+        }
+        if (!userRepository.existsById(id)){
+            log.error("Couldn't find the user you were looking for.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,String.format("Could not find user with id %s",id));
+        }
+        if (user.getPassword().length() <= 16){
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+        }
+        user.setUserId(id);
+        userRepository.save(user);
+    }
+
 }
